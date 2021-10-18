af1c1186e3b6bba99239798127ad4ddc0d11851a
Nikita Rehemae <nikita.rehemae@gmail.com>
2021-10-18 15:36:11 +0300

    Commit for Bogdan <3

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,30 @@
 @extends('layout')
 @section('title', 'Posts')
 @section('content')
-    <div class="row">
-        <div class="col">
-            @if($posts->previousPageUrl())
-                <a href="{{ $posts->previousPageUrl() }}" class="btn btn-primary">Previous</a>
-            @else
-                <a href="{{ $posts->previousPageUrl() }}" class="btn btn-primary disabled" role="button" aria-disabled="true">Previous</a>
-            @endif
-        </div>
-        <div class="col text-end">
-            @if($posts->nextPageUrl())
-                <a href="{{ $posts->nextPageUrl() }}" class="btn btn-primary">Next</a>
-            @else
-                <a href="{{ $posts->nextPageUrl() }}" class="btn btn-primary disabled" role="button" aria-disabled="true">Next</a>
-            @endif
-        </div>
-    </div>
+{{--    <div class="row">--}}
+{{--        <div class="col">--}}
+{{--            @if($posts->previousPageUrl())--}}
+{{--                <a href="{{ $posts->previousPageUrl() }}" class="btn btn-primary">Previous</a>--}}
+{{--            @else--}}
+{{--                <a href="{{ $posts->previousPageUrl() }}" class="btn btn-primary disabled" role="button" aria-disabled="true">Previous</a>--}}
+{{--            @endif--}}
+{{--        </div>--}}
+{{--        <div class="col text-end">--}}
+{{--            @if($posts->nextPageUrl())--}}
+{{--                <a href="{{ $posts->nextPageUrl() }}" class="btn btn-primary">Next</a>--}}
+{{--            @else--}}
+{{--                <a href="{{ $posts->nextPageUrl() }}" class="btn btn-primary disabled" role="button" aria-disabled="true">Next</a>--}}
+{{--            @endif--}}
+{{--        </div>--}}
+{{--    </div>--}}
+    {{$posts->links()}}
     <div class="row row-cols-4">
         @foreach($posts as $post)
             <div class="col">
                 <div class="card mt-3">
+                    @if($post->image_path)
+                        <img src="{{$post->image_path}}" class="card-img-top">
+                    @endif
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <p class="card-text">{{ $post->snippet }}</p>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -12,6 +12,17 @@
             <label for="body" class="form-label">Content</label>
             <textarea class="form-control" id="body" rows="15" name="body">{{$post->body}}</textarea>
         </div>
+        @error('image')
+        @foreach($errors->get('image') as $error))
+        <div class="alert alert-danger" role="alert">
+            {{ $error }}
+        </div>
+        @endforeach
+        @enderror
+        <div class="mb-3">
+            <label for="image" class="form-label">Image</label>
+            <input type="file" class="form-control" id="image" name="image" accept="image/*">
+        </div>
         <input type="submit" class="btn btn-primary" value="Edit">
         <a class="btn btn-primary" href="{{url()->previous()}}">Back</a>
     </form>
